29dd5c8011828795816d27e5471978e57ab78261
Kirill Davydychev <kdavyd@amazon.com>
2022-07-20 20:58:58 -0500

    fixed indentation.

--- src/singleton.py
+++ src/singleton.py
@@ -18,35 +18,35 @@
 
 
 class SingletonClass(object):
-  """ALL CLI ACTIONS SCOPED"""
-  AWS_ACCOUNT_ID = None # Account ID of the user. Retrieved using boto3 which reads AWS CLI config
-  AWS_USER_ID = None #Short account specific ID
-  AWS_CANONICAL_USER_ID = None #Long cross account unique ID
-  AWS_ORIGIN_REGION = None #Region where data originates from
-  AWS_DEST_REGION = None #Region where data is copied to
-  NUM_JOBS = None #Number jobs to be run in parallel
-  FULL_COPY = None #Create full copy of snapshot at additional cost (more through)
-  S3_BUCKET = None #S3 bucket where snapshots are stored or will be stored in
-  VERBOSITY_LEVEL = None #-1 quite. 1,2,3 for v, vv, vvv respectively
-  DRY_RUN = None #Run a FSP Action, only checking permissions
-  NODEPS = None #Skip Dependency Checks
-  SUPPRESS_WRITES = None #Script will not produce log files
+    """ALL CLI ACTIONS SCOPED"""
+    AWS_ACCOUNT_ID = None # Account ID of the user. Retrieved using boto3 which reads AWS CLI config
+    AWS_USER_ID = None #Short account specific ID
+    AWS_CANONICAL_USER_ID = None #Long cross account unique ID
+    AWS_ORIGIN_REGION = None #Region where data originates from
+    AWS_DEST_REGION = None #Region where data is copied to
+    NUM_JOBS = None #Number jobs to be run in parallel
+    FULL_COPY = None #Create full copy of snapshot at additional cost (more through)
+    S3_BUCKET = None #S3 bucket where snapshots are stored or will be stored in
+    VERBOSITY_LEVEL = None #-1 quite. 1,2,3 for v, vv, vvv respectively
+    DRY_RUN = None #Run a FSP Action, only checking permissions
+    NODEPS = None #Skip Dependency Checks
+    SUPPRESS_WRITES = None #Script will not produce log files
 
-  """"Some Project Scoped Constants"""
-  RETRY_BLOCK_COUNT = 10
-  RETRY_RANGE_COUNT = 3
-  RETRY_JOB_COUNT = 1 #Job is same as range
+    """"Some Project Scoped Constants"""
+    RETRY_BLOCK_COUNT = 10
+    RETRY_RANGE_COUNT = 3
+    RETRY_JOB_COUNT = 1 #Job is same as range
 
-  """"Global Variables for getfroms3 and movetos3"""
-  ENDPOINT_URL = None
-  PROFILE = None
+    """"Global Variables for getfroms3 and movetos3"""
+    ENDPOINT_URL = None
+    PROFILE = None
 
-  """Global Variables for getfroms3 and movetos3"""
-  AWS_S3_ENDPOINT_URL = None
-  AWS_S3_PROFILE = None
-
-  def __new__(cls):
-    if not hasattr(cls, 'instance'):
-      cls.instance = super(SingletonClass, cls).__new__(cls)
-    return cls.instance
+    """Global Variables for getfroms3 and movetos3"""
+    AWS_S3_ENDPOINT_URL = None
+    AWS_S3_PROFILE = None
+    
+    def __new__(cls):
+      if not hasattr(cls, 'instance'):
+        cls.instance = super(SingletonClass, cls).__new__(cls)
+      return cls.instance
 
